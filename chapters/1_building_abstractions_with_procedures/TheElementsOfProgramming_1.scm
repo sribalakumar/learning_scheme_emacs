ec8b30af10e47290655de89d6f65a04903ee1898
Bala <sribalakumar@gmail.com>
2015-09-07 17:56:49 -0400

    Left over topics in 1.1

--- chapters/1_building_abstractions_with_procedures/TheElementsOfProgramming_1.scm
+++ chapters/1_building_abstractions_with_procedures/TheElementsOfProgramming_1.scm
@@ -101,8 +101,6 @@
       ; Predicates can be procedures or expressions that evaluates to true or false
       ; <predicate1> is evaluated first, if it is false then <predicate2> is evaluted. If <predicate2> is false then <predicate3> is evaluted.
       ; if all predicates evalutes to false then the expression corresponding to else predicate is evaluted.
-
-    ; Using cond conditional statement:
         (define (abs x)
           (cond ((< x 0) (- x))
                 (else x))) ; represents mod |x| function
@@ -127,3 +125,93 @@
       ; Above snippet using not
         (define (>= x y)
           (not (< x y)))
+
+;1.1.7 Example: Square Roots by Newton's Method
+    ; square root of (x)  = y such that y >= 0 and y^2 = x
+      (define (square x) (* x x))
+    ; In computer science we use imperative(how to) descriptions of functions.
+    ; Newton's method of successive repetation says that whenever we have a guess y for square root of x,
+    ;   we arrive at a better guess by averaging y with x/y.
+    ; |Guess|        |Quotient|            |Average|
+    ;    1             (2/1)=2               ((2+1)/2) = 1.5
+    ;    1.5           (2/1.5)=1.333         ((1.333+1.5)/2) = 1.4167
+    ;    1.4167        (2/1.4167)=1.4118     ((1.4167+1.4118)/2) = 1.4142
+    ;    1.4142        ....                  ....
+    ; continuing the process we obtatin better approximations to the square root.
+      (define (sqrt-iter guess x)
+         (if (good-enough? guess x)
+             guess
+             (sqrt-iter (improve guess x)
+                        x)))
+    ; averaging the quotient of the radicand and the old guess
+      (define (improve guess x)
+        (average guess (/ x guess))
+    ; where we define average as
+      (define (average x y)
+        (/ (+ x y) 2))
+    ; For simplicity as of now we define good enough such that its square differs from the radicand by less than a predetermined tolerance 0.001
+      (define (good-enough? guess x)
+        (< (abs (- (square guess) x)) 0.001))
+    ;We need to start with a guess and lets start with 1
+      (define (sqrt x)
+        (sqrt-iter 1.0 x))
+
+    ; Calling examples to sqrt
+      (sqrt 9) ; 3.00009155413138
+      (sqrt (+ 100 37)) ; 11.704699917758145
+      (sqrt (+ (sqrt 2) (sqrt 3))) ; 1.7739279023207892
+      (square (sqrt 1000)) ; 1000.000369924366
+
+    ; sqrt-iter demonstrates how iteration can be accompanied using no special construct other then ordinary ability to call a procedure.
+    ; Example .1.6 is a must for pratice
+
+;1.1.8 Procedures as Black-Box Abstractions
+    ; Every indvidual have their own implementation of squaring
+      ; A lame man can compute square like
+        (define (square x) (* x x))
+      ; A math guy can define it like
+        (define (square x)
+          (exp (double (log x))))
+        (define (double x) (+ x x))
+      ; Each of those definitions is fine and when we have to import a library then the first implementation should not affect the final result.
+        ; Such abstraction of a procedure is called procedural abstraction. The users of a procedure may not have written it themselves, but may have
+        ; obtained it from another programmer as a black box. A user should not need to know how the procedure is implemented in order to use it.
+
+    ; Local Names
+      ; The implementers choice of names for the procedure's formal parameters should not matter to the user of the procedure.
+        (define (square x) (* x x))
+        (define (good-enough? guess x)
+          (< (abs (- (square guess) x)) 0.001))
+      ; here the parameter x in square is isloated to its own procedure and is not the same parameter x of procedure good-enough 
+      ; In the good-enough? procedure - guess, x are bounded to its procedure and are called bound variable with the procedure good-enough has their
+        ; scope. If a variable is not bound, we say that it is free. The set of expressions for which binding defines a name is called the scope of 
+        ; that name.
+
+    ; Internal definitions and block structure
+      ; Formal parameters of a procedure are local to the body of the procedure.
+      ; Existing sqrt procedure can be decomposed into various procedures like sqrt-iter, good-enough?, improve, average.
+      ; Problem is in a large system there can  numerous libraries which can lead to name conflicts with the procedures.
+      ; Hence to localize the procedures with in the main procedure:
+        (define (sqrt x)
+          (define (good-enough? guess x)
+            (< (abs (- (square guesss) x)) 0.001))
+          (define (improve guess x)
+            (average guess (/ x guesss)))
+          (define (sqrt-iter guess x)
+            (if (good-enough? guess x)
+                guess
+                (sqrt-iter (improve guess x) x)))
+          (sqrt-iter 1.0 x)
+        ; This type of nesting of definitions is called block structure.
+      ; Caveat in the above definition is passing paramter x even though it is available in the block, so it is not necessary to explicityly mention x
+        ; This discipline is called lexical scoping.
+        (define (sqrt x)
+          (define (good-enough? guess)
+            (< (abs (- (square guess) x)) 0.001))
+          (define (improve guess)
+             (average guess (/ x guess)))
+          (define (sqrt-iter guess)
+             (if (good-enough? guess)
+                 guess
+                 (sqrt-iter (improve guess))))
+          (sqrt-iter 1.0))
